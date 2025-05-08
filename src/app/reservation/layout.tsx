a635be0b52db33a529e8e7674a1d016ed9f3bf4e
David Stafa <david.stafa@gmail.com>
2025-05-08 22:14:33 +0200

    feat: refactor form components and improve layout for reservation page

--- src/app/reservation/layout.tsx
+++ src/app/reservation/layout.tsx
@@ -1,12 +1,14 @@
+import Container from "@/components/container";
+
 export default function ReservationLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex h-full flex-col items-center justify-center">
-      <h1 className="text-center">Rezervace celoročních setů</h1>
+    <Container>
+      <h1 className="text-center text-lg font-medium my-4">Rezervační formulář</h1>
       {children}
-    </div>
+    </Container>
   );
 }
