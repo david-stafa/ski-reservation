7a68775f75a5e34048f313ae9317eb9ad91114b4
David Stafa <david.stafa@gmail.com>
2025-05-11 20:32:49 +0200

    feat: create new reservation actions

--- src/app/_actions/reservationActions.ts
+++ src/app/_actions/reservationActions.ts
@@ -22,6 +22,30 @@ export async function getAllReservationsDates(date: string) {
   });
 }
 
-export async function getSumOfReservations(){
-  return await prisma.reservation.count()
+export async function getSumOfReservations() {
+  return await prisma.reservation.count();
+}
+
+export async function getReservationById(id: string) {
+  return await prisma.reservation.findUnique({
+    where: {
+      id: id,
+    },
+  });
+}
+
+export async function getAllReservations() {
+  return await prisma.reservation.findMany({
+    select: {
+      startDate: true,
+      endDate: true,
+      firstName: true,
+      lastName: true,
+      email: true,
+      phone: true,
+      peopleCount: true,
+      id: true,
+      createdAt: true,
+    },
+  });
 }
