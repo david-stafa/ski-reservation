bf8f27e65e8cbadc51843d793785c6dbf392f601
David Stafa <david.stafa@gmail.com>
2025-05-22 21:55:20 +0200

    feat: enable user to edit reservatio dateTime within already reserved hours

--- src/app/reservation/[id]/page.tsx
+++ src/app/reservation/[id]/page.tsx
@@ -1,9 +1,10 @@
-import { deleteReservation } from "@/app/_actions/reservation/formActions";
 import { getReservationById } from "@/app/_actions/reservation/reservationActions";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { DeleteReservationButton } from "./components/DeleteReservationButton";
 import { format } from "date-fns";
+import { DateTime } from "luxon";
+import { cn } from "@/lib/utils";
 
 type ReservationDetailPageProps = {
   params: Promise<{ id: string }>;
@@ -13,15 +14,28 @@ const ReservationDetailPage = async ({
   params,
 }: ReservationDetailPageProps) => {
   const { id } = await params;
-  const reservation = await getReservationById(id);
-  console.log(reservation);
+  const reservation = async () => {
+    const data = await getReservationById(id);
+    const formattedData = {
+      ...data,
+      startDate: DateTime.fromJSDate(data?.startDate, {
+        zone: "Europe/Prague",
+      }).toFormat("dd.M.yyyy"),
+      startTime: DateTime.fromJSDate(data?.startDate, {
+        zone: "Europe/Prague",
+      }).toFormat("HH:mm"),
+      endTime: DateTime.fromJSDate(data?.endDate, {
+        zone: "Europe/Prague",
+      }).toFormat("HH:mm"),
+    };
+    return formattedData;
+  };
+  const reservationData = await reservation();
 
-  if (!reservation) {
+  if (!reservationData) {
     return (
-      <div className="flex flex-col items-center justify-center h-screen gap-8">  
-        <div className="text-xl font-semibold">
-          Rezervace nenalezena
-        </div>
+      <div className="flex flex-col items-center justify-center h-screen gap-8">
+        <div className="text-xl font-semibold">Rezervace nenalezena</div>
         <Link href={`/`}>
           <Button variant="default">Vrátit se na domovskou stránku</Button>
         </Link>
@@ -35,36 +49,33 @@ const ReservationDetailPage = async ({
         Detail vaší rezervace
       </h1>
       <div className="space-y-3 mb-8">
+        <DetailComponent className="text-lg font-semibold">
+          <span className="text-zinc-500">Datum rezervace:</span>{" "}
+          <span className="font-medium">{reservationData.startDate}</span>
+        </DetailComponent>
+        <DetailComponent className="text-lg font-semibold">
+          <span className="text-zinc-500">Čas rezervace:</span>{" "}
+          <span className="font-medium">
+            {reservationData.startTime} - {reservationData.endTime}
+          </span>
+        </DetailComponent>
         <DetailComponent>
           <span className="text-zinc-500">Jméno:</span>{" "}
           <span className="font-medium">
-            {reservation.firstName} {reservation.lastName}
+            {reservationData.firstName} {reservationData.lastName}
           </span>
         </DetailComponent>
         <DetailComponent>
           <span className="text-zinc-500">Email:</span>{" "}
-          <span className="font-medium">{reservation.email}</span>
+          <span className="font-medium">{reservationData.email}</span>
         </DetailComponent>
         <DetailComponent>
           <span className="text-zinc-500">Telefon:</span>{" "}
-          <span className="font-medium">{reservation.phone}</span>
+          <span className="font-medium">{reservationData.phone}</span>
         </DetailComponent>
         <DetailComponent>
           <span className="text-zinc-500">Počet osob:</span>{" "}
-          <span className="font-medium">{reservation.peopleCount}</span>
-        </DetailComponent>
-        <DetailComponent>
-          <span className="text-zinc-500">Datum rezervace:</span>{" "}
-          <span className="font-medium">
-            {format(reservation.startDate, "dd.MM.yyyy")}
-          </span>
-        </DetailComponent>
-        <DetailComponent>
-          <span className="text-zinc-500">Čas rezervace:</span>{" "}
-          <span className="font-medium">
-            {format(reservation.startDate, "HH:mm")} -{" "}
-            {format(reservation.endDate, "HH:mm")}
-          </span>
+          <span className="font-medium">{reservationData.peopleCount}</span>
         </DetailComponent>
       </div>
       <div className="flex gap-4 justify-center">
@@ -79,6 +90,14 @@ const ReservationDetailPage = async ({
 
 export default ReservationDetailPage;
 
-const DetailComponent = ({ children }: { children: React.ReactNode }) => {
-  return <p className="mb-1 text-base flex items-center gap-2">{children}</p>;
+type DetailComponentProps = React.HTMLAttributes<HTMLParagraphElement> & {
+  children: React.ReactNode;
+};
+
+const DetailComponent = ({ children, className }: DetailComponentProps) => {
+  return (
+    <p className={cn("mb-1 text-base flex items-center gap-2", className)}>
+      {children}
+    </p>
+  );
 };
