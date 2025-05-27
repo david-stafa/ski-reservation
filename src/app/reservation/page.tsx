ee47ccee25894fb8a0ac0ff10fc855f533f40bbb
David Stafa <david.stafa@gmail.com>
2025-05-27 21:26:44 +0200

    feat: update homepage logo rendering and add header to reservation page

--- src/app/reservation/page.tsx
+++ src/app/reservation/page.tsx
@@ -4,6 +4,7 @@ import Form from "../components/Form/Form";
 export default function ReservationPage() {
   return (
     <Container>
+      <h1 className="text-2xl font-bold">Vytvořit rezervaci</h1>
       <Form />
     </Container>
   );
