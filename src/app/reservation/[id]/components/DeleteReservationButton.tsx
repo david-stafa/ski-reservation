8ec7ceb0fa6b89f178f0919748f9463d564aaabc
David Stafa <david.stafa@gmail.com>
2025-06-16 08:06:16 +0200

    feat: implement delete reservation modal

--- src/app/reservation/[id]/components/DeleteReservationButton.tsx
+++ src/app/reservation/[id]/components/DeleteReservationButton.tsx
@@ -2,27 +2,73 @@
 
 import { deleteReservation } from "@/app/_actions/reservation/formActions";
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { XCircleIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export function DeleteReservationButton({ id }: { id: string }) {
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  // TODO: add success message?
 
   const handleDelete = async () => {
-    const response = await deleteReservation(id);
-    console.log("Deleting reservation with ID:", id);
-    if (response.success) {
-      // Redirect to the reservations page or show a success message
-      window.location.href = "/";
-    } else {
-      // Handle error
-      console.error(response.error);
+    try {
+      setIsDeleting(true);
+      await deleteReservation(id);
+      router.push("/");
+    } catch {
+      setError(
+        "Nastala chyba při mazání rezervace. Prosím zkuste to znovu později."
+      );
+    } finally {
+      setIsDeleting(false);
     }
   };
 
-
-  // TODO: Add confirmation modal with confirmation button and cancel button
-
   return (
-    <Button variant="destructive" onClick={handleDelete}>
-      Smazat rezervaci
-    </Button>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="destructive">Smazat rezervaci</Button>
+      </DialogTrigger>
+      <DialogContent className="text-center">
+        <DialogHeader>
+          <XCircleIcon className="size-15 mx-auto text-destructive " />
+          <DialogTitle className="text-lg font-semibold">
+            Jste si jisti?
+          </DialogTitle>
+          <DialogDescription className="text-sm text-zinc-500">
+            Opravdu chcete smazat rezervaci? Tato akce je nevratná.
+          </DialogDescription>
+        </DialogHeader>
+        {error && <p className="text-red-500 italic text-sm">{error}</p>}
+        <DialogFooter className="flex flex-row !justify-center gap-2 mt-2">
+          <DialogClose asChild>
+            <Button variant="outline" className="px-10">
+              Zrušit
+            </Button>
+          </DialogClose>
+          <Button
+            variant="destructive"
+            className="max-w-xs"
+            onClick={handleDelete}
+            disabled={isDeleting}
+          >
+            Smazat rezervaci
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
