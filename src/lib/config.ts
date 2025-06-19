df01e025e1691cdba8bca4ed555a55301a67e682
David Stafa <david.stafa@gmail.com>
2025-06-19 22:14:44 +0200

    fix: email config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,11 +1,9 @@
 export const config = {
-  isProduction: process.env.NODE_ENV === 'production',
-  isDevelopment: process.env.NODE_ENV === 'development',
   email: {
     from: 'Ski Reservation <onboarding@resend.dev>',
     subject: 'Potvrzení rezervace',
     // In development, always send to your email for testing
     getToEmail: (userEmail: string) => 
-      process.env.NODE_ENV === 'production' ? userEmail : 'david.stafa@gmail.com'
+      process.env.EMAIL_ENV === 'production' ? userEmail : 'david.stafa@gmail.com'
   }
-}; 
+};
