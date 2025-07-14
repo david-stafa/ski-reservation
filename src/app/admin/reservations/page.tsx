15de186d9ab3a7c5d21e06c221f6a11c6be619cd
David Stafa <david.stafa@gmail.com>
2025-07-14 11:16:50 +0200

    feat: add admin page and enhance reservations page with logout button and user info

--- src/app/admin/reservations/page.tsx
+++ src/app/admin/reservations/page.tsx
@@ -1,16 +1,26 @@
 import { getCachedAllReservations } from "@/app/_actions/reservation/reservationActions";
+import { LogOutButton } from "@/components/auth/logout/logOutButton";
 import Container from "@/components/container";
+import HeadingUnderline from "@/components/headingUnderline";
+import { getUser } from "@/lib/dal";
 import { columns } from "./columns";
 import { DataTable } from "./data-table";
 
 const Reservations = async () => {
   const reservations = await getCachedAllReservations();
+  const user = await getUser();
+  console.log(user);
 
   return (
     <Container>
-      <h1>Reservations</h1>
-      <p>This is the reservations page.</p>
-      <div className="container mx-auto py-10">
+      <div className="relative">
+        <h1 className="text-2xl font-bold mb-4 text-center">
+          Přehled rezervací
+        </h1>
+        <LogOutButton />
+      </div>
+      <HeadingUnderline />
+      <div className="container mx-auto pb-10">
         <DataTable columns={columns} data={reservations} />
       </div>
     </Container>
