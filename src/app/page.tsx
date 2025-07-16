8c5220a58bfb08b3494647ab42d7ad99131d4489
David Stafa <david.stafa@gmail.com>
2025-07-16 15:55:24 +0200

    fix: layout and styling in landing page components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ export default async function Home() {
 
   return (
     // TODO: h-[100dvh] is not supported on older browsers - find some fallback or something
-    <Container className="flex flex-col h-[100dvh] justify-center w-full gap-8 relative max-w-2xl">
-      <div className="w-[240px] h-[42px] relative">
+    <Container className="flex flex-col h-[100dvh] justify-between md:justify-center w-full gap-8 xl:gap-12 relative max-w-2xl">
+      <div className="w-[200px] md:w-[240px] h-[36px] md:h-[42px] relative">
         <Image
           src="/logo.png"
           alt="Ski Logo"
@@ -23,13 +23,17 @@ export default async function Home() {
           priority
         />
       </div>
-      <h1 className="font-bold text-2xl">Rezervujte si celoroční výpůjčku lyžařského setu</h1>
-      <p className="text-zinc-600">
-        Rezervujte si termín a dorazte si k nám vyzkoušet a zamluvit lyžařský
-        set na celou zimní sezónu 2025/2026
-      </p>
+      <section className="flex flex-col gap-4">
+        <h1 className="font-bold text-2xl">
+          Rezervujte si celoroční výpůjčku lyžařského setu
+        </h1>
+        <p className="text-zinc-600">
+          Rezervujte si termín a dorazte si k nám vyzkoušet a zamluvit lyžařský
+          set na celou zimní sezónu 2025/2026
+        </p>
+      </section>
       <AvailabilityDisplay reservations={reservations} />
-      <div className="w-full absolute bottom-0 left-0 p-4">
+      <div className="w-full mt-8 xl:mt-10">
         <Link href="/reservation">
           <Button variant="default" size="lg" className="w-full mb-3">
             Vytvořit rezervaci
