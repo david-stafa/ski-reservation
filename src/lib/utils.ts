aa3fecffdfc15586bf5b6ce10d54838af0099087
David Stafa <david.stafa@gmail.com>
2025-09-08 18:12:43 +0200

    style: format code for consistency and improve readability in utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,16 +1,15 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 // TODO - finish phone number format
-export function phoneNumFormat(telNum:string | undefined){
+export function phoneNumFormat(telNum: string | undefined) {
+  if (telNum == undefined) return;
 
-  if (telNum == undefined) return
-
-  let formattedNum: string[] = telNum.split("")
+  let formattedNum: string[] = telNum.split("");
 
   if (formattedNum.length == 3) {
     formattedNum = telNum.split("");
