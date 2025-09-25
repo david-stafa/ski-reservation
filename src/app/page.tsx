ff56733f96ab30d1ec5fa4909283251c704e874c
David Stafa <david.stafa@gmail.com>
2025-09-25 19:03:13 +0200

    feat: handle state when ski seets count reached limit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,41 @@ import Image from "next/image";
 import Link from "next/link";
 import Container from "../components/container";
 import { getCachedSumOfReservations } from "./_actions/reservation/reservationActions";
+import { SKI_SETS_LIMIT } from "@/lib/constants";
+import { Link2 } from "lucide-react";
 
 export default async function Home() {
   // Fetch data from the server
   const reservations = await getCachedSumOfReservations();
+
+  if (reservations._total >= SKI_SETS_LIMIT) {
+    return (
+      <Container className="p-5 flex flex-col h-[100dvh] md:justify-center w-full gap-8 xl:gap-12 relative max-w-2xl">
+        <div className="w-[200px] md:w-[240px] h-[36px] md:h-[42px] relative">
+          <Image
+            src="/logo.png"
+            alt="Ski Logo"
+            fill
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            className="scale-down"
+            priority
+          />
+        </div>
+        <section className="flex flex-col my-auto md:my-0 gap-4">
+          <h1 className="font-bold text-2xl">
+            Celoroční sety jsou na tuto sezónu vyprodány.
+          </h1>
+          <p className="text-zinc-600">Těšíme se na vás v příští sezóně!</p>
+          <Link href="https://skiblazek.cz" className="cursor-pointer">
+            <Button variant="default" size="lg" className="px-10 py-3 mb-3 cursor-pointer">
+              <Link2 />
+              skiblazek.cz
+            </Button>
+          </Link>
+        </section>
+      </Container>
+    );
+  }
 
   return (
     // TODO: h-[100dvh] is not supported on older browsers - find some fallback or something
@@ -32,6 +63,12 @@ export default async function Home() {
         </p>
       </section>
       <AvailabilityDisplay reservations={reservations} />
+      <p className="text-zinc-600">
+        Počet dostupných celoročních setů:{" "}
+        <span className="font-semibold">
+          {SKI_SETS_LIMIT - reservations._total}
+        </span>
+      </p>
       <div className="w-full mt-8 xl:mt-10">
         <Link href="/reservation">
           <Button variant="default" size="lg" className="w-full mb-3">
--- src/app/reservation/page.tsx
+++ src/app/reservation/page.tsx
@@ -1,9 +1,25 @@
 import Container from "@/components/container";
-
 import HeadingUnderline from "@/components/headingUnderline";
 import Form from "@/components/reservationForm/Form";
+import { getCachedSumOfReservations } from "../_actions/reservation/reservationActions";
+import { SKI_SETS_LIMIT } from "@/lib/constants";
 
-export default function ReservationPage() {
+export default async function ReservationPage() {
+  const reservations = await getCachedSumOfReservations();
+
+  // if the limit of reservation is reached, return this component
+  if (reservations._total >= SKI_SETS_LIMIT) {
+    return (
+      <Container className="max-w-2xl p-5 md:m-5 border-zinc-200 md:border rounded-lg md:shadow-md">
+        <h1 className="text-2xl font-bold text-center mb-4">
+          Celoroční sety jsou na tuto sezónu vyprodány.
+        </h1>
+        <h2 className="text-center">Těšíme se na vás v příští sezóně!</h2>
+      </Container>
+    );
+  }
+
+  // base component with the form
   return (
     <Container className="max-w-2xl p-5 md:m-5 border-zinc-200 md:border rounded-lg md:shadow-md">
       <h1 className="text-2xl font-bold text-center mb-4">
