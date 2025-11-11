9e9c1163549ff5f3bc18d556f819e92682d0a89a
David Stafa <david.stafa@gmail.com>
2025-11-11 18:36:12 +0100

    fix: missing constant

--- src/app/seasonal-reservation/page.tsx
+++ src/app/seasonal-reservation/page.tsx
@@ -1,14 +1,14 @@
 import Container from "@/components/container";
 import HeadingUnderline from "@/components/headingUnderline";
 import SeasonalReservationForm from "@/components/reservationForm/seasonalReservationForm/SeasonalReservationForm";
-import { SKI_SETS_LIMIT } from "@/lib/constants";
+import { SEASONAL_SKI_SETS_LIMIT } from "@/lib/constants";
 import { getCachedSumOfSeasonalReservations } from "../_actions/seasonalReservation/seasonalReservationActions";
 
 export default async function SeasonalReservationPage() {
   const reservations = await getCachedSumOfSeasonalReservations();
 
   // if the limit of reservation is reached, return this component
-  if (reservations._total >= SKI_SETS_LIMIT) {
+  if (reservations._total >= SEASONAL_SKI_SETS_LIMIT) {
     return (
       <Container className="max-w-2xl p-5 md:m-5 border-zinc-200 md:border rounded-lg md:shadow-md">
         <h1 className="text-2xl font-bold text-center mb-4">
