97413827e26379fdd81b178efcfb93b65b86c22b
David Stafa <david.stafa@gmail.com>
2025-12-15 10:20:37 +0100

    refactor: clean up admin layout and page structure

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -6,13 +6,11 @@ const AdminLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       <nav className="mx-auto p-5 bg-blue-100 mb-5">
-        <div className="container flex items-center gap-2 mx-auto justify-between px-5">
-            <Link href="/admin">
-              <Button size="sm">
-                Domů
-              </Button>
-            </Link>
-            <LogOutButton />
+        <div className="container flex items-center gap-2 mx-auto justify-between">
+          <Link href="/admin">
+            <Button size="sm">Domů</Button>
+          </Link>
+          <LogOutButton />
         </div>
       </nav>
       {children}
